fe88fdc7f9450daa43fa2cd7285b715631e1b41e
Debbie Liknes <55964159+debbie-liknes@users.noreply.github.com>
2025-03-29 22:02:52 -0400

    Fixed some normals

--- ChaiGraphics/ChaiEngine/source/Renderables/Cube.cpp
+++ ChaiGraphics/ChaiEngine/source/Renderables/Cube.cpp
@@ -4,32 +4,45 @@ namespace chai::brew
 {
 	CubeRO::CubeRO()
 	{
+		//unshared vertices, for flat shading
 		m_vertexData = createVertexBuffer<glm::vec3>(PrimDataType::FLOAT, 3);
 		AddVertexBuffer(m_vertexData, 0);
-		glm::vec3 p1 = { 0.5f,  0.5f, -0.5f };
-		glm::vec3 p2 = { 0.5f, -0.5f, -0.5f };
-		glm::vec3 p3 = { -0.5f, -0.5f, -0.5f };
-		glm::vec3 p4 = { -0.5f,  0.5f, -0.5f };
+		glm::vec3 p0 = { 0.5f,  0.5f, -0.5f };
+		glm::vec3 p1 = { 0.5f, -0.5f, -0.5f };
+		glm::vec3 p2 = { -0.5f, -0.5f, -0.5f };
+		glm::vec3 p3 = { -0.5f,  0.5f, -0.5f };
 
-		glm::vec3 p5 = { 0.5f,  0.5f, 0.5f };
-		glm::vec3 p6 = { 0.5f, -0.5f, 0.5f };
-		glm::vec3 p7 = { -0.5f, -0.5f, 0.5f };
-		glm::vec3 p8 = { -0.5f,  0.5f, 0.5f };
-		m_vertexData->data = { p1, p2, p3, p4, p5, p6, p7, p8 };
+		glm::vec3 p4 = { 0.5f,  0.5f, 0.5f };
+		glm::vec3 p5 = { 0.5f, -0.5f, 0.5f };
+		glm::vec3 p6 = { -0.5f, -0.5f, 0.5f };
+		glm::vec3 p7 = { -0.5f,  0.5f, 0.5f };
+		m_vertexData->data = { 
+			p4, p5, p6, p7, //facing +z
+			p0, p1, p2, p3, //facing -z
+			p0, p3, p4, p7,	//facing +y
+			p1, p2, p5, p6,	//facing -y
+			p0, p1, p4, p5,	//facing +x
+			p2, p3, p6, p7	//facing -x
+		};
 
 		//normals
 		m_normalData = createVertexBuffer<glm::vec3>(PrimDataType::FLOAT, 3);
 		AddVertexBuffer(m_vertexData, 1);
-		glm::vec3 n1 = { 0.f,  1.f, 0.0f };
-		glm::vec3 n2 = { 0.f, -1.f, 0.0f };
-		glm::vec3 n3 = { 0.f, -1.f, 0.0f };
-		glm::vec3 n4 = { 0.f,  1.f, 0.0f };
+		glm::vec3 n0 = { 0.f,  0.f, 1.f };
+		glm::vec3 n1 = { 0.f, 0.f, -1.f };
+		glm::vec3 n2 = { 0.f, 1.f, 0.f };
+		glm::vec3 n3 = { 0.f,  -1.f, 0.f };
+		glm::vec3 n4 = { 1.f,  0.f, 0.f };
+		glm::vec3 n5 = { -1.f, 0.f, 0.f };
 
-		glm::vec3 n5 = { 0.f,  1.f, 0.0f };
-		glm::vec3 n6 = { 0.f, -1.f, 0.0f };
-		glm::vec3 n7 = { 0.f, -1.f, 0.0f };
-		glm::vec3 n8 = { 0.f,  1.f, 0.0f };
-		m_normalData->data = { n1, n2, n3, n4, n5, n6, n7, n8 };
+		m_normalData->data = { 
+			n0, n0, n0, n0, 
+			n1, n1, n1, n1, 
+			n2, n2, n2, n2, 
+			n3, n3, n3, n3, 
+			n4, n4, n4, n4, 
+			n5, n5, n5, n5
+		};
 
 		m_colorData = createUniformBuffer<glm::vec3>(PrimDataType::FLOAT);
 		AddUniform(m_colorData, 1);
@@ -39,12 +52,12 @@ namespace chai::brew
 		m_indexBuffer = createIndexBuffer();
 		AddIndexBuffer(m_indexBuffer, 1);
 		m_indexBuffer->data = { 
-			0, 1, 3, 1, 2, 3, //front face
-			7, 6, 4, 5, 4, 6, 
-			0, 3, 4, 7, 4, 3,
-			3, 2, 6, 7, 3, 6,
-			1, 0, 5, 4, 5, 0,
-			6, 1, 5, 1, 6, 2
+			1, 0, 3, 2, 1, 3,
+			6, 7, 4, 4, 5, 6, 
+			8, 9, 10, 10, 9, 11,
+			13, 12, 14, 13, 14, 15,
+			17, 16, 18, 17, 18, 19,
+			21, 20, 22, 21, 22, 23
 		};
 
 		RequestViewData();
