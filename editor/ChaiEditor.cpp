23452654f5db45f26bd685e2223fbd293706afa6
Debbie Liknes <dkliknes@gmail.com>
2026-07-20 01:27:21 -0400

    Fixed blending

--- editor/ChaiEditor.cpp
+++ editor/ChaiEditor.cpp
@@ -114,9 +114,9 @@ int main()
     auto scene = std::make_unique<Scene>();
 
 
-    //auto prefab = models->load(makeAssetId("model:sponza"), assetDir() /
-    //"Sponza/intel/main_sponza/NewSponza_Main_glTF_003.glTF");
-    auto prefab = models->load(makeAssetId("model:sponza"), assetDir() / "Sponza/glTF/Sponza.gltf");
+    auto prefab = models->load(makeAssetId("model:sponza"), assetDir() /
+    "Sponza/intel/main_sponza/NewSponza_Main_glTF_003.glTF");
+    //auto prefab = models->load(makeAssetId("model:sponza"), assetDir() / "Sponza/glTF/Sponza.gltf");
     //auto prefab = models->load(makeAssetId("model:sponza"), assetDir() / "ABeautifulGame/glTF/ABeautifulGame.gltf");
 
     if (prefab) {
