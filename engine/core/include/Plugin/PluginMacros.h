bcc4108b2322a83488fc531f553399e2d1ac23ab
Lucius Latham <kingcodefish@gmail.com>
2026-07-19 23:49:20 -0400

    Fix plugin extern C issue; add logic for linux loading of libraries and module lookups; add executable dir lookup in linux (#26)

--- engine/core/include/Plugin/PluginMacros.h
+++ engine/core/include/Plugin/PluginMacros.h
@@ -3,21 +3,16 @@
 
 #define CHAI_PLUGIN_ABI_VERSION 1
 
-#define CHAI_PLUGIN_API
-//#if defined(_WIN32)
-//#define CHAI_PLUGIN_API __declspec(dllexport)
-//#else
-//#define CHAI_PLUGIN_API 
-//#endif
-
-
 //Place in a .cpp, not header
 #define CHAI_PLUGIN(Type)                                                                          \
-    CHAI_PLUGIN_API int chaiPluginAbiVersion()                                                     \
+    extern "C"                                                                                     \
     {                                                                                              \
-        return CHAI_PLUGIN_ABI_VERSION;                                                            \
-    }                                                                                              \
-    CHAI_PLUGIN_API ::chai::IPlugin* chaiCreatePlugin()                                            \
-    {                                                                                              \
-        return new Type();                                                                         \
+        int chaiPluginAbiVersion()                                                                 \
+        {                                                                                          \
+            return CHAI_PLUGIN_ABI_VERSION;                                                        \
+        }                                                                                          \
+        ::chai::IPlugin* chaiCreatePlugin()                                                        \
+        {                                                                                          \
+            return new Type();                                                                     \
+        }                                                                                          \
     }
