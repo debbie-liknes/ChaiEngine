ddb86b8ee5bd267f8f42ae6a0abb26ce3b231947
Lucius Latham <kingcodefish@gmail.com>
2026-08-14 15:59:33 -0400

    Use std::span for building plugin graph

--- engine/runtime/source/Plugin/PluginLoader.cpp
+++ engine/runtime/source/Plugin/PluginLoader.cpp
@@ -17,7 +17,7 @@ namespace chai
         constexpr auto kManifestExt = ".json";
     } // namespace
     
-    graph::Graph<IPlugin*> buildPluginGraph(std::vector<IPlugin*>& plugins,
+    graph::Graph<IPlugin*> buildPluginGraph(std::span<IPlugin*>,
                                             const IPlugin::ServiceList& providedServices);
 
     IPlugin* PluginLoader::load(const fs::path& manifestPath)
@@ -100,7 +100,7 @@ namespace chai
         return true;
     }
 
-    graph::Graph<IPlugin*> buildPluginGraph(std::vector<IPlugin*>& plugins,
+    graph::Graph<IPlugin*> buildPluginGraph(std::span<IPlugin*> plugins,
                                             const IPlugin::ServiceList& providedServices)
     {
         graph::Graph<IPlugin*> pluginGraph;
